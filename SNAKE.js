77b92361a950752affeeb4ef107f5f9a2f0c745c
holly6a31 <64143966+holly6a31@users.noreply.github.com>
2020-05-06 23:19:15 +0900

    Add files via upload

--- SNAKE.js
+++ SNAKE.js
@@ -8,26 +8,26 @@ const columns = canvas.width / scale;
 var fruit_image;
   fruit_image = new Image();
   fruit_image.src = "bear.jpg";
-  
-  var snake;
+
+  let fruit = {
+    x : Math.floor(Math.random()*17+1) * scale,
+    y : Math.floor(Math.random()*15+3) * scale
+    }
+
+    let score = 0;
+
+    var snake;
   (function setup() {
-   snake = new Snake();
+ snake = new Snake();
 
- let fruit = {
-  x : Math.floor(Math.random()*17+1) * scale,
-  y : Math.floor(Math.random()*15+3) * scale
-  }
 
-  let score = 0;
-
-  
  window.setInterval(() => {
    ctx.clearRect(0, 0, canvas.width, canvas.height);
    snake.update();
    snake.draw();
    ctx.drawImage (fruit_image, fruit.x, fruit.y, 30, 30);
-  ctx.font = "45px Changa one";
-  ctx.fillText(score,2*scale,4*scale);
+   ctx.font = "45px Changa one";
+   ctx.fillText(score,2*scale,4*scale); 
 }, 250);
 }());
 
