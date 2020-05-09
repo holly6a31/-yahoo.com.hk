000913cd344af9f6c1d39fb71a9d8cdaac341a63
holly6a31 <64143966+holly6a31@users.noreply.github.com>
2020-05-09 21:34:28 +0900

    Add files via upload

--- SNAKE.js
+++ SNAKE.js
@@ -4,30 +4,43 @@ const scale = 10;
 const rows = canvas.height / scale;
 const columns = canvas.width / scale;
 
-
-var fruit_image;
-  fruit_image = new Image();
-  fruit_image.src = "bear.jpg";
+ function fruit(){
+  fruit.x ;
+  fruit.y;   
+   
+  fruit_image.pickLocation = function(){
+    fruit.x = (Math.floor(Math.random() * rows - 1) + 1) * scale;
+    fruit.y = (Math.floor(Math.random() * columns -1) +1) * scale;
+   }
   
-  var snake;
-  (function setup() {
-   snake = new Snake();
-
- let fruit = {
-  x : Math.floor(Math.random()*17+1) * scale,
-  y : Math.floor(Math.random()*15+3) * scale
+   var fruit_image;
+  
+   fruit_image.draw = function(){
+    fruit_image = new Image();
+    fruit_image.src = "bear.jpg";
+   }
   }
 
-  let score = 0;
+    let score = 0;
 
-  
+    var snake;
+  (function setup() {
+ snake = new Snake();
+ fruit = new fruit();
+ fruit_image.pickLocation; 
+
  window.setInterval(() => {
    ctx.clearRect(0, 0, canvas.width, canvas.height);
+   fruit_image.draw();
    snake.update();
-   snake.draw();
+   snake.draw()
    ctx.drawImage (fruit_image, fruit.x, fruit.y, 30, 30);
-  ctx.font = "45px Changa one";
-  ctx.fillText(score,2*scale,4*scale);
+   ctx.font = "45px Changa one";
+   ctx.fillText(score,2*scale,4*scale);
+   
+   if (snake.eat(fruit)){
+    fruit_image.pickLocation;  
+   } 
 }, 250);
 }());
 
