e2c6e4ee4296c2ce5e877d599a458f5067f13628
holly6a31 <64143966+holly6a31@users.noreply.github.com>
2020-05-10 00:24:00 +0900

    Add files via upload

--- WEB.js
+++ WEB.js
@@ -1,57 +1,78 @@
 function Snake(){
-   this.x = 0;
-   this.y = 0;
-   this.xSpeed = scale * 1;
-   this.ySpeed = 0;
+  this.x = 0;
+  this.y = 0;
+  this.xSpeed = scale * 1;
+  this.ySpeed = 0;
+  this.total = 0;
+  this.tail = [];
 
-   this.draw = function(){
-      ctx.fillStyle = "lightgreen";
-      ctx.fillRect(this.x, this.y, 80, 80);
+  this.draw = function(){
+     ctx.fillStyle = "lightgreen";
+     for (let i=0; i<this.tail.length; i++){
+      ctx.fillRect(this.tail[i].x, this. tail[i].y, scale, scale);
       ctx.strokestyle = "darkgreen";
-      ctx.strokeRect(this.x, this.y, 80, 80);
-    
-    }
-
-    this.update = function(){
-    this.x += this.xSpeed;
-    this.y += this.ySpeed;
-
-    if (this.x > canvas.width){
-     this.x = 0;
-    }
+      ctx.strokeRect(this.tail[i].x, this.tail[i].y, scale, scale);
    
-    if(this.y > canvas.height){
-      this.y = 0;
     }
-
-    if(this.x < 0){
-        this.x = canvas.width;
-    }
-
-    if(this.y < 0){
-        this.y = canvas.width;
-    }
-
- }
-
- this.changeDirection = function(direction){
-   switch(direction){
-     case "Up":
-    this.xSpeed = 0;
-    this.ySpeed = -scale * 1;
-    break;
-    case "Down":
-        this.xSpeed = 0;
-        this.ySpeed = scale * 1;
-        break;
-    case "Left":
-        this.xSpeed =  -scale * 1;
-        this.ySpeed = 0;
-        break;
-    case "Right":
-        this.xSpeed = scale * 1;
-        this.ySpeed = 0;
-        break;
+     ctx.fillRect(this.x, this.y, scale, scale);
+     ctx.strokestyle = "darkgreen";
+     ctx.strokeRect(this.x, this.y, scale, scale);
+   
    }
- }
+
+   this.update = function(){
+   for (let i=0; i<this.tail.length -1; i++){
+      this.tail[i] = this.tail[i+1];
+   }
+
+   this.tail[this.total - 1] = { x: this.x, y: this.y};
+  
+   this.x += this.xSpeed;
+   this.y += this.ySpeed;
+
+   if (this.x > canvas.width){
+    this.x = 0;
+   }
+  
+   if(this.y > canvas.height){
+     this.y = 0;
+   }
+
+   if(this.x < 0){
+       this.x = canvas.width;
+   }
+
+   if(this.y < 0){
+       this.y = canvas.width;
+   }
+
+}
+
+this.changeDirection = function(direction){
+  switch(direction){
+    case "Up":
+   this.xSpeed = 0;
+   this.ySpeed = -scale * 1;
+   break;
+   case "Down":
+       this.xSpeed = 0;
+       this.ySpeed = scale * 1;
+       break;
+   case "Left":
+       this.xSpeed =  -scale * 1;
+       this.ySpeed = 0;
+       break;
+   case "Right":
+       this.xSpeed = scale * 1;
+       this.ySpeed = 0;
+       break;
+  }
+}
+this.eat = function(fruit){
+  if(this.x === fruit.x && this.y === fruit.y){
+    this.total++;
+    return true;
+  }
+  return false;
+}
 }
