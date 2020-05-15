ac8707f1259f136663aa5a7d5ed822ea5cc51711
holly6a31 <64143966+holly6a31@users.noreply.github.com>
2020-05-16 00:15:41 +0900

    Add files via upload

--- SNAKE.js
+++ SNAKE.js
@@ -4,31 +4,28 @@ const scale = 10;
 const rows = canvas.height / scale;
 const columns = canvas.width / scale;
 
-
-var fruit_image;
-  fruit_image = new Image();
-  fruit_image.src = "bear.jpg";
-  
-  var snake;
+    var snake;
+    
   (function setup() {
-   snake = new Snake();
+  snake = new Snake();
+  fruit = new Fruit();
+  fruit.pickLocation(); 
+ 
 
- let fruit = {
-  x : Math.floor(Math.random()*17+1) * scale,
-  y : Math.floor(Math.random()*15+3) * scale
-  }
-
-  let score = 0;
-
-  
- window.setInterval(() => {
-   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  window.setInterval(() => {
+   ctx.clearRect(0, 0, canvas.width, canvas.height); 
+   fruit.draw();
    snake.update();
    snake.draw();
-   ctx.drawImage (fruit_image, fruit.x, fruit.y, 30, 30);
-  ctx.font = "45px Changa one";
-  ctx.fillText(score,2*scale,4*scale);
-}, 250);
+   
+   if (snake.eat(fruit)){
+    fruit.pickLocation();  
+   }
+   snake.checkCollision();
+   document.querySelector(".score")
+   .innerText = snake.total;
+   
+}, 150);
 }());
 
 window.addEventListener("keydown", ((evt) => {
